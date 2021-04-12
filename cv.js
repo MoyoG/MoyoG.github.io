9b24cf9fee4f481a920244bdf055f838fa58d76c
Munkhjargal Gochoo (Moyo) <mgl229@yahoo.com>
2021-04-12 14:14:22 +0400

    Add files via upload

--- cv.js
+++ cv.js
@@ -8,10 +8,195 @@ function hide(x){
 	q.style.display="none";
 }
 
+function sortTableyear(id,itab,id2) {
+	var id2=id2;
+	var itab=itab;
+	var a = itab%2;
+	var table, rows, switching, i, x, y, shouldSwitch;
+	table = document.getElementById(id);
+	switching = true;
+	rows = table.rows;
+	/*Make a loop that will continue until
+	no switching has been done:*/
+	if (a != 0){	while (switching) {
+		itab=2;
+		//start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		document.getElementById(id2).className = "sorting_asc";
+		/*Loop through all table rows (except the
+		first, which contains table headers):*/
+		for (i = 1; i < (rows.length - 1); i++) {
+		  //start by saying there should be no switching:
+		  shouldSwitch = false;
+		  /*Get the two elements you want to compare,
+		  one from current row and one from the next:*/
+		  x = rows[i].getElementsByTagName("TD")[0];
+		  y = rows[i + 1].getElementsByTagName("TD")[0];
+		  //check if the two rows should switch place:
+		  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+			//if so, mark as a switch and break the loop:
+			shouldSwitch = true;
+			break;
+		  }
+		}
+		if (shouldSwitch) {
+		  /*If a switch has been marked, make the switch
+		  and mark that a switch has been done:*/
+		  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		  switching = true;
+		}
+	  }
+	}
+	if (a == 0){	while (switching) {
+		itab=1;
+		//start by saying: no switching is done:
+		switching = false;
+		rows = table.rows;
+		document.getElementById(id2).className = "sorting_desc";
+		/*Loop through all table rows (except the
+		first, which contains table headers):*/
+		for (i = 1; i < (rows.length - 1); i++) {
+		  //start by saying there should be no switching:
+		  shouldSwitch = false;
+		  /*Get the two elements you want to compare,
+		  one from current row and one from the next:*/
+		  x = rows[i].getElementsByTagName("TD")[0];
+		  y = rows[i + 1].getElementsByTagName("TD")[0];
+		  //check if the two rows should switch place:
+		  if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+			//if so, mark as a switch and break the loop:
+			shouldSwitch = true;
+			break;
+		  }
+		}
+		if (shouldSwitch) {
+		  /*If a switch has been marked, make the switch
+		  and mark that a switch has been done:*/
+		  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+		  switching = true;
+		}
+	  }
+	}
+
+  }
+
+function sortTabletype(id,itype,id2) {
+	var id2=id2 
+	var itype = itype;
+	var b = itype%2;
+	var table, rows, switching, i, x, y, shouldSwitch;
+	table = document.getElementById(id);
+	switching = true;
+	rows = table.rows;
+
+	/*Make a loop that will continue until
+	no switching has been done:*/
+	if (b != 0){ while (switching) {
+			//start by saying: no switching is done:
+			switching = false;
+			rows = table.rows;
+			document.getElementById(id2).className = "sorting_asc";
+			/*Loop through all table rows (except the
+			first, which contains table headers):*/
+			for (i = 1; i < (rows.length - 1); i++) {
+			  //start by saying there should be no switching:
+			  shouldSwitch = false;
+			  /*Get the two elements you want to compare,
+			  one from current row and one from the next:*/
+			  x = rows[i].getElementsByTagName("TD")[1];
+			  y = rows[i + 1].getElementsByTagName("TD")[1];
+			  //check if the two rows should switch place:
+			  if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+				//if so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			  }
+			}
+			if (shouldSwitch) {
+			  /*If a switch has been marked, make the switch
+			  and mark that a switch has been done:*/
+			  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			  switching = true;
+			}
+		  }
+	}
+	if (b == 0){ while (switching) {
+			//start by saying: no switching is done:
+			switching = false;
+			rows = table.rows;
+			document.getElementById(id2).className = "sorting_desc";
+			/*Loop through all table rows (except the
+			first, which contains table headers):*/
+			for (i = 1; i < (rows.length - 1); i++) {
+			  //start by saying there should be no switching:
+			  shouldSwitch = false;
+			  /*Get the two elements you want to compare,
+			  one from current row and one from the next:*/
+			  x = rows[i].getElementsByTagName("TD")[1];
+			  y = rows[i + 1].getElementsByTagName("TD")[1];
+			  //check if the two rows should switch place:
+			  if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+				//if so, mark as a switch and break the loop:
+				shouldSwitch = true;
+				break;
+			  }
+			}
+			if (shouldSwitch) {
+			  /*If a switch has been marked, make the switch
+			  and mark that a switch has been done:*/
+			  rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+			  switching = true;
+			}
+		  }
+	}
+  }
+
+function category(x){
+	if (x == 'all-papers'){
+		document.getElementById('all-papers').style.display="inline";
+		document.getElementById('selected-papers').style.display = "none";
+		document.getElementById('DL-papers').style.display="none";
+		document.getElementById('Elder-papers').style.display = "none";
+		document.getElementById('embedded-papers').style.display = "none";
+	}
+	if (x == 'selected-papers'){
+		document.getElementById('all-papers').style.display="none";
+		document.getElementById('selected-papers').style.display = "inline";
+		document.getElementById('DL-papers').style.display="none";
+		document.getElementById('Elder-papers').style.display = "none";
+		document.getElementById('embedded-papers').style.display = "none";
+	}
+	if (x == 'DL+ML-papers'){
+		document.getElementById('all-papers').style.display="none";
+		document.getElementById('selected-papers').style.display = "none";
+		document.getElementById('DL-papers').style.display="inline";
+		document.getElementById('Elder-papers').style.display = "none";
+		document.getElementById('embedded-papers').style.display = "none";
+	}
+	if (x == 'Elder-papers'){
+		document.getElementById('all-papers').style.display="none";
+		document.getElementById('selected-papers').style.display = "none";
+		document.getElementById('DL-papers').style.display="none";
+		document.getElementById('Elder-papers').style.display = "inline";
+		document.getElementById('embedded-papers').style.display = "none";
+	}
+	if (x == 'embedded-papers'){
+		document.getElementById('all-papers').style.display="none";
+		document.getElementById('selected-papers').style.display = "none";
+		document.getElementById('DL-papers').style.display="none";
+		document.getElementById('Elder-papers').style.display = "none";
+		document.getElementById('embedded-papers').style.display = "inline";
+	}
+
+}
 function togglePopup(x){
   document.getElementById(x).classList.toggle("active");
 }
-
+function cete() {
+	( this ).addClass( 'active' ).removeClass( 'active' );
+		  
+};
 var objPeople =[
 					{
 						username:"munkhjargal",
